b007d8bf56622c7e3ef91c091b4571a56d17adb6
1jdsteele <jdsteelepersonal@gmail.com>
2025-04-13 19:19:11 -0700

    side menu dynamically created. sorting functionality working. 30+ mons in program. renamed json to metaMons. mstly met requirements currently functional.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,15 @@
-
-
-
 import { Pokemon } from './Pokemon.js';
+// vars needed by multiple functions
 let loadedPokemon = []; //keeping a list of all the pokemon we are supposed to show
+let currentSortStat = "id"; //in beginning show by id, ascending :)
+let currentSortOrder = "asc"; 
 
+
+//++++++++++ Section 1:loading and showing mons +++++++
 //this function akin to showCards
 //load
 async function loadPokemonData() { //must be marked async bc next 2 lines - allows me to await
-  const response = await fetch('./metaRestricteds.json'); //async function, return a promise. If don't use await, get promise object instead of response
+  const response = await fetch('./metaMons.json'); //async function, return a promise. If don't use await, get promise object instead of response
   const jsonData = await response.json();// another async func - await pauses the function execution until promise resolved
   // this is in lieu of .then s
 
@@ -21,11 +23,19 @@ async function loadPokemonData() { //must be marked async bc next 2 lines - allo
     loadedPokemon.push(pokemon);
 
   }
-  publishCardsFromList();
+  sortAndDisplayPokemon()
 }
 
 //parse
 function buildPokemonFromRaw(raw) {
+
+  const sprite =
+  raw.sprites?.other?.["official-artwork"]?.front_default ||
+  raw.sprites?.front_default ||
+  ""; //bc cornerstone mask did not have front sprite... whatever, official artwork better anyways
+
+// console.log("Building:", raw.name, "| Sprite:", sprite); //testing bc cornerstone mask doesn't have front sprite - just official artwork
+
   return new Pokemon({
     name: raw.name,
     id: raw.id,
@@ -40,7 +50,10 @@ function buildPokemonFromRaw(raw) {
     specialDefense: raw.stats.find(s => s.stat.name === "special-defense").base_stat,
     speed: raw.stats.find(s => s.stat.name === "speed").base_stat,
     weight: raw.weight,
-    spritesFront: raw.sprites.front_default
+    // spritesFront: raw.sprites.front_default
+    // spritesFront: raw.sprites.front_default || raw.sprites.other["official-artwork"]?.front_default || ""
+    spritesFront: sprite
+
   });
 }
 
@@ -62,7 +75,12 @@ function publishCardsFromList() { //this one akin to editCardContent
     list.innerHTML = `
       <li>Type: ${pokemon.type1}${pokemon.type2 ? ' / ' + pokemon.type2 : ''}</li>
       <li>HP: ${pokemon.hp}</li>
+      <li>Attack: ${pokemon.attack}</li>
+      <li>Defense: ${pokemon.defense}</li>
+      <li>Special Attack: ${pokemon.specialAttack}</li>
+      <li>Special Defense: ${pokemon.specialDefense}</li>
       <li>Speed: ${pokemon.speed}</li>
+      <li>Weight: ${pokemon.weight}</li>
     `;
   
     cardContainer.appendChild(newCard);
@@ -71,6 +89,102 @@ function publishCardsFromList() { //this one akin to editCardContent
 }
 
 
+
+//+++++++++section 2: sorting +++++++++
+
+function sortAndDisplayPokemon() {
+  loadedPokemon.sort((a, b) => {
+    const valA = a[currentSortStat];
+    const valB = b[currentSortStat];
+
+    if (typeof valA === "string" && typeof valB === "string") {
+      return currentSortOrder === "asc"
+        ? valA.localeCompare(valB)
+        : valB.localeCompare(valA);
+    }
+
+    //IF TIE we sort by id
+    if (valA === valB) {
+      return a.id - b.id;
+    }
+
+    return currentSortOrder === "asc" ? valA - valB : valB - valA;
+  });
+
+
+
+
+  // now that we have the order we want, clear html and publish list
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.innerHTML = "";
+  publishCardsFromList();
+}
+
+
+
+function buildSideMenu() {
+  // a local array and map to help organize
+  // an array of what we want to sort by
+  const sortingParams = [
+    "name", "id", "hp", "attack", "defense", "specialAttack", "specialDefense", "speed", "weight"
+  ]
+  //map of what should be displayed based on what the stat is called in data
+  const sortingDisplayNames = {
+    name: "Name",
+    id: "ID",
+    hp: "HP",
+    attack: "Attack",
+    defense: "Defense",
+    specialAttack: "Sp Atk",
+    specialDefense: "Sp Def",
+    speed: "Speed",
+    weight: "Weight"
+  }
+  
+  //now we begin in earnest
+  //begin by clearing the default side menu
+  const menu = document.getElementById("side-menu");
+  menu.innerHTML = ""; 
+
+
+  for (const stat of sortingParams) {
+    const slotDiv = document.createElement("div");
+    slotDiv.classList.add("side-menu-slots");
+
+    const ascButton = document.createElement("button");
+    ascButton.classList.add("sort-ascending");
+    ascButton.textContent = "ASC";
+    ascButton.onclick = () => {
+      currentSortStat = stat;
+      currentSortOrder = "asc";
+      sortAndDisplayPokemon();
+    }
+
+    const statLabel = document.createElement("div");
+    statLabel.classList.add("sorting-parameter");
+    statLabel.textContent = sortingDisplayNames[stat]; //using our map :)
+
+    const descButton = document.createElement("button");
+    descButton.classList.add("sort-descending");
+    descButton.textContent = "DSC";
+    descButton.onclick = () => {
+      currentSortStat = stat;
+      currentSortOrder = "desc";
+      sortAndDisplayPokemon();
+    }
+
+    slotDiv.appendChild(ascButton);
+    slotDiv.appendChild(statLabel);
+    slotDiv.appendChild(descButton);
+
+    menu.appendChild(slotDiv);
+  }
+}
+
+
+
+
+// ++++++++++++ section 4: other functions ++++++++++
 //so these last two click event functions silently failed. I am going to fix them
 //when I turned this file from a text/javascript to a module all of the onclick functions are no longer global so they began to silently fail.
 
@@ -95,5 +209,15 @@ window.removeLastCard = function () { //eventually this will transform into a wa
 }
 
 
+
+
+
+
+
+
+
 // Run when page is loaded
-document.addEventListener("DOMContentLoaded", loadPokemonData);
+document.addEventListener("DOMContentLoaded", () => {
+  loadPokemonData();
+  buildSideMenu();
+});
